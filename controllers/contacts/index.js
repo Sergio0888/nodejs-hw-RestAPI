2fdee1dea7005b7292f925d94a7e2eb8d9c21eb3
Sergei Maznoi <s.maznoi888@gmail.com>
2022-11-09 16:49:11 +0200

    add new change (Mongo.DB)

---
+++ controllers/contacts/index.js
@@ -0,0 +1,15 @@
+const getAll = require("./getAll")
+const getById = require("./getById")
+const add = require("./add")
+const updateById = require("./updateById")
+const updateStatusById = require("./updateStatusById")
+const removeById = require("./removeById")
+
+module.exports = {
+    getAll,
+    getById,
+    add,
+    updateById,
+    updateStatusById,
+    removeById,
+}
